1e2ea6b1718a50e2b58e5141bb1753d5e8391168
clewczyk <clewczyk@ucsc.edu>
2014-11-06 20:18:08 -0800

    Updated index and do_costdivide.js

---
+++ Project/js/do_costdivide.js
@@ -0,0 +1,26 @@
+
+function do_costdivide() 
+{
+	var num_people = parseInt(document.costdivideform.num_people.value);
+	if(isNaN(num_people) || num_people <= 0){
+		alert("Please enter a valid amount of people");
+		return false;
+	}
+	var total_cost = parseInt(document.costdivideform.total_cost.value);
+	if(isNaN(total_cost) || total_cost <= 0){
+		alert("Please enter a valid total cost");
+		return false;
+	}
+	var result = (total_cost/num_people).toFixed(2);
+	
+	alert("The cost per person is $" + result);
+	return false;
+}
+
+
+
+function do_test()
+{
+	alert("This is a test");
+	return false;
+}
