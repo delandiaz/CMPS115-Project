c2a8e777bbe6824f14eeaa4c0adbe58e4b6a7ec6
clewczyk <clewczyk@ucsc.edu>
2014-11-10 22:55:05 -0800

    Update index and do_costdivide to create venmo url

--- Project/js/do_costdivide.js
+++ Project/js/do_costdivide.js
@@ -3,7 +3,8 @@ var result;
 
 function do_costdivide() 
 {
-	//var len = document.costdivideform.
+	var len = document.costdivideform.Field5.length;
+	
 	var num_people = parseInt(document.costdivideform.num_people.value);
 	if(isNaN(num_people) || num_people <= 0){
 		alert("Please enter a valid amount of people");
@@ -15,9 +16,35 @@ function do_costdivide()
 		return false;
 	}
 	result = (total_cost/num_people).toFixed(2);
+	url();
 	alert("The cost per person is $" + result);
 	return false;
 }
+
+
+function url(){
+  var url = "https://venmo.com/";
+  var helper = "?txn=pay&amount=";
+  var helper2 = "&note=";
+  var user_name = document.getElementById("venmo_id").value;
+  var comments = document.getElementById("comment_id").value;
+  comments = comments.trim();
+  if(comments == null || comments == "")
+  {
+	url += user_name + helper + result;
+  }
+  else
+  {
+	var comments2 = comments.split(' ').join('+');
+  //alert(user_name);
+  //url += user_name + helper + result;
+	url += user_name + helper + result + helper2 + comments2;
+   }
+  alert(url);
+  //return false;
+    
+}
+
 
 
 function do_test()
